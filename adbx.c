a9c2127de92f7e5c9f2999c880b2f50e5b2339d9
Daniel Colascione <dancol@dancol.org>
2014-05-11 17:19:26 -0700

    tweak subcommand dispatch

--- adbx.c
+++ adbx.c
@@ -14,20 +14,22 @@ extern int shex_main(int, char**);
 int
 real_main(int argc, char** argv)
 {
+    int (*sub_main)(int, char**) = NULL;
+
     if (argc < 2)
         die(EINVAL, "no sub-command given");
 
-    int (*sub_main)(int, char**) = NULL;
-    if (!strcmp(argv[1], "stub"))
+    if (sub_main == NULL && !strcmp(argv[1], "stub"))
         sub_main = stub_main;
-    else if (!strcmp(argv[1], "shex"))
+
+    if (sub_main == NULL && (!strcmp(argv[1], "shex") ||
+                             !strcmp(argv[1], "sh")))
         sub_main = shex_main;
 
-    if (sub_main) {
-        argv[1] = xaprintf("%s %s", prgname, argv[1]);
-        set_prgname(argv[1]);
-        return sub_main(argc - 1, argv + 1);
-    }
+    if (sub_main == NULL)
+        die(EINVAL, "unknown sub-command %s", argv[1]);
 
-    die(EINVAL, "unknown sub-command %s", argv[1]);
+    argv[1] = xaprintf("%s %s", prgname, argv[1]);
+    set_prgname(argv[1]);
+    return sub_main(argc - 1, argv + 1);
 }
